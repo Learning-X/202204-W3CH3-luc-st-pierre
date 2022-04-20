f2399d2d56655aed0afc18a5d6fea7bf49e7ef01
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-20 22:20:48 +0200

    Added seriesAPI data to PageComponent main entry point

--- site/public/js/components/PageComponent.js
+++ site/public/js/components/PageComponent.js
@@ -1,6 +1,9 @@
+import seriesAPI from "../../data/series.js";
 import Component from "./Component";
 
 export default class PageComponent extends Component {
+  series = seriesAPI;
+
   constructor(parentElement) {
     super(parentElement, "conatainer");
     this.generateHTML();
