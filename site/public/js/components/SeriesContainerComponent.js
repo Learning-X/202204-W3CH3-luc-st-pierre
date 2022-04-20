6e8e29150fc44f21b9bebda004ac0e2300bd7774
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-21 01:02:04 +0200

    Created a state object for holding series status passed to constructor

--- site/public/js/components/SeriesContainerComponent.js
+++ site/public/js/components/SeriesContainerComponent.js
@@ -1,7 +1,21 @@
+import Component from "./Component.js";
+
 export default class SeriesContainerComponent extends Component {
-  constructor(parentElement, className, series) {
+  state = {};
+
+  constructor(parentElement, className, series, status) {
     super(parentElement, className, "section");
 
+    this.state.status = status;
     this.generateHTML();
+  }
+
+  generateHTML() {
+    this.element.innerHTML = `
+      <h3 class="subsection-title">Pending series</h3>
+         <p class="info">You have 4 series pending to watch</p>           <!--<p class="info">Congrats! You've watched all your series</p>-->
+       <ul class="series-list">
+       </ul>
+    `;
   }
 }
