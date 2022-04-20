65a8136796ff239aec0186f6307c5732df43e2c5
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-20 21:40:38 +0200

    Added default fallback to div for htmlTag to prevent undefined

--- site/public/js/components/Component.js
+++ site/public/js/components/Component.js
@@ -1,7 +1,7 @@
 export default class Component {
   element;
 
-  constructor(parentElement, className, htmlTag) {
+  constructor(parentElement, className, htmlTag = "div") {
     this.element = document.createElement(this.element);
     this.element.className = className;
     parentElement.append(this.element);
